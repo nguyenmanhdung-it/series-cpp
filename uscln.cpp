d14112f58bdf2767ea774f66f24d4b92d28f9a41
dung-editor <manhdung1682003@gmail.com>
2021-11-18 09:57:44 +0700

    baitap

--- uscln.cpp
+++ uscln.cpp
@@ -1,19 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main ()
+int main()
 {
-     int M,N;
-     cout<<"M=";
-     cin >> M;
-     cout<<"N=";
-     cin>>N;
-   
-    while(M!=N)
+    int M, N;
+    cout << " nhap hai so M, N : \n";
+    cout << "M=";
+    cin >> M;
+    cout << "N=";
+    cin >> N;
+    while (M != N)
     {
-        if (M>N)
-            M=M-N;
-        else 
-            N=N-M;
+        if (M > N)
+        {
+            M = M - N;
+        }
+        else
+            N = N - M;
     }
+    cout << "uscln = " << M;
     return 0;
-}   
+}
